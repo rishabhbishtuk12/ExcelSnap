a1f30d90b90532e41796ccc76541eba0e38403a3
Rishabh Bisht <224134759+rishabhbishtuk12@users.noreply.github.com>
2026-02-01 16:46:44 +0530

    Add gitigone

--- app.py
+++ app.py
@@ -148,4 +148,6 @@ def process_file():
 #     return jsonify({"message": "Done"})
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
